8edbb46032222d3d53fff546f3241cc7234bac72
uki00a <uki00a@gmail.com>
2022-09-15 07:04:37 +0900

    fix: parse `deno info --json`

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,26 @@
 // @ts-check
-import { restoreCache, saveCache } from '@actions/cache'
+import { getExecOutput } from '@actions/exec'
+import { z } from 'zod'
 
-console.info(restoreCache, saveCache)
+const denoInfo = z.object({
+  denoDir: z.string(),
+  modulesCache: z.string(),
+  typescriptCache: z.string(),
+  registryCache: z.string(),
+  originStorage: z.string()
+})
+
+async function main() {
+  const denoExecutable = 'deno'
+  const { stdout } = await getExecOutput(denoExecutable, ['info', '--json'], {
+    silent: true,
+    failOnStdErr: true
+  })
+  const info = denoInfo.parse(JSON.parse(stdout.trim()))
+  console.info(info)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
