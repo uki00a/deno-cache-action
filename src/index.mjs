943ee4decc39b1e8e5328d2454a99f3d7277f2ba
uki00a <uki00a@gmail.com>
2022-09-16 07:08:21 +0900

    feat: support import map

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,20 @@
 // @ts-check
+import { readFile } from "node:fs/promises";
 import { restoreCache, saveCache } from "@actions/cache";
 import { getInput, getMultilineInput } from "@actions/core";
 import { exec, getExecOutput } from "@actions/exec";
 import { z } from "zod";
 
-const denoInfo = z.object({
+const denoInfoSchema = z.object({
   denoDir: z.string(),
   modulesCache: z.string(),
   typescriptCache: z.string(),
   registryCache: z.string(),
   originStorage: z.string(),
+});
+
+const importMapSchema = z.object({
+  imports: z.record(z.string()).optional(),
 });
 
 async function main() {
@@ -18,8 +23,9 @@ async function main() {
     silent: true,
     failOnStdErr: true,
   });
-  const info = denoInfo.parse(JSON.parse(stdout.trim()));
+  const info = denoInfoSchema.parse(JSON.parse(stdout.trim()));
   const entrypoints = getMultilineInput("path", { trimWhitespace: true });
+  const maybeImportMap = getInput("import-map", { trimWhitespace: true });
   const key = getInput("key", { trimWhitespace: true, required: true });
   const toCache = [
     [info.modulesCache, `${key}-modules-cache`],
@@ -29,14 +35,49 @@ async function main() {
   }
 
   for (const entrypoint of entrypoints) {
-    await exec(denoExecutable, ["cache", "--quiet", entrypoint], {
-      failOnStdErr: true,
+    await denoCache({
+      executable: denoExecutable,
+      entrypoint,
+      importMap: maybeImportMap,
     });
+  }
+
+  if (maybeImportMap) {
+    const importMap = importMapSchema.parse(
+      JSON.parse(
+        (await readFile(maybeImportMap, { encoding: "utf-8" })).trim(),
+      ),
+    );
+    const imports = importMap.imports ?? {};
+    for (const specifier of Object.keys(imports)) {
+      const isPartialSpecifier = specifier.endsWith("/");
+      if (isPartialSpecifier) continue;
+      await denoCache({
+        executable: denoExecutable,
+        entrypoint: imports[specifier],
+      });
+    }
   }
 
   for (const [path, key] of toCache) {
     await saveCache([path], key);
   }
+}
+
+/**
+ * @param {{ executable: string, importMap?: string, entrypoint: string }} options
+ */
+async function denoCache(options) {
+  const { executable: denoExecutable, importMap: maybeImportMap, entrypoint } =
+    options;
+  const args = ["cache", "--quiet"];
+  if (maybeImportMap) {
+    args.push("--import-map", maybeImportMap);
+  }
+  args.push(entrypoint);
+  await exec(denoExecutable, args, {
+    failOnStdErr: true,
+  });
 }
 
 main().catch((error) => {
